824f8cbddbaac779d7a76d24aa412f53253d4be3
dhruvvyas951 <61121089+dhruvvyas951@users.noreply.github.com>
2020-05-09 14:51:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var gameState = 0;
+var form,player,game;
+var playerCount;
+var database;
+
+function setup(){
+    createCanvas(500,500);
+
+    database = firebase.database();
+
+    game = new Game();
+    game.getState();
+    game.start();
+
+}
+
+function draw(){
+    background("white");
+
+    drawSprites();
+}
